6dd260edbeaaacb1250de04caf808dfcde7a1939
Mike Hughes <mike@initiat.io>
2019-12-06 00:05:35 +0000

    Day 2 complete

--- day-2/advent-2.js
+++ day-2/advent-2.js
@@ -1,22 +1,27 @@
-const intCodeProgram = prgArray => {
-  let currentIndex = 0;
-  while (prgArray[currentIndex] != 99) {
-    let code = prgArray[currentIndex];
-    let value1 = prgArray[currentIndex + 1];
-    let value2 = prgArray[currentIndex + 2];
-    let write = prgArray[currentIndex + 3];
-    switch (code) {
-      case 1:
-        prgArray[write] = prgArray[value1] + prgArray[value2];
-        currentIndex += 4;
-        break;
-      case 2:
-        prgArray[write] = prgArray[value1] * prgArray[value2];
-        currentIndex += 4;
-        break;
-    }
-  }
-  return prgArray;
+const intCodeProgram = (memory, noun, verb) => {
+	let currentAddress = 0;
+	memory[1] = noun;
+	memory[2] = verb;
+	while (memory[currentAddress] != 99) {
+		let instruction = memory[currentAddress];
+		let parameter1 = memory[currentAddress + 1];
+		let parameter2 = memory[currentAddress + 2];
+		let parameter3 = memory[currentAddress + 3];
+		switch (instruction) {
+		case 1:
+			memory[parameter3] = memory[parameter1] + memory[parameter2];
+			currentAddress += 4;
+			break;
+		case 2:
+			memory[parameter3] = memory[parameter1] * memory[parameter2];
+			currentAddress += 4;
+			break;
+		default:
+			break;
+		}
+	}
+
+	return memory;
 };
 
 exports.intCodeProgram = intCodeProgram;
